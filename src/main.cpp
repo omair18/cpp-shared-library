f9507eb4755b6e90d1cec8a62c8752e8887b34f8
omair18 <omair.hassaan@gmail.com>
2020-02-13 17:13:37 +0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "pal.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (1)
+    {
+        char buffer[64] = {0};
+        cin >> buffer;
+
+        if (isPalindrome(buffer))
+        {
+            cout << "Word is a palindrome" << endl;
+        }
+        else
+        {
+            cout << "Word is not a palindrome" << endl;
+        }
+    }
+
+    return 0;
+}
